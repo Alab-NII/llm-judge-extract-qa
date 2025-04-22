65ba47be3708b4d464026cf7b916d436a099881a
xanhho <xanhhocntt@gmail.com>
2025-04-22 14:22:59 +0900

    Update ReadMe

--- README.md
+++ README.md
@@ -2,6 +2,24 @@
 
 This is the repository for the paper: [LLM-as-a-Judge: Reassessing the Performance of LLMs in Extractive QA](https://arxiv.org/abs/2504.11972)
 
+
+
+## Reproduction of Results
+- download [data.zip](https://www.dropbox.com/scl/fi/pmxaxt5ibj9mlzui5gbpw/data.zip?rlkey=dc2nuie1ad47kkk7ta8slfklk&st=6pycuere&dl=0)
+- download [judge.zip](https://www.dropbox.com/scl/fi/5i88veb30xqbbj7bn0w71/judge.zip?rlkey=xobwku0k5m70vw74y3ssmztih&st=8n8eyb9b&dl=0) 
+
+### Table 2: Pearson correlation coefficients
+```bash
+python3 get_correlation_score.py
+```
+
+### Table 3: Automatic evaluation scores (EM and F1) and LLM-as-a-judge scores
+```bash
+python3 run_eval.py
+```
+
+
+
 ## Running process
 
 ### Step 1: Run the QA Task
@@ -34,10 +52,9 @@ python3 get_correlation_score.py
 - data/human_result.json: human judgement dataset
 - [qa_inference](https://www.dropbox.com/scl/fi/fe56j1m358tppcb14mjqx/qa_inference.zip?rlkey=15wdi626sebivuhu80b63pae2&st=v5cbz03q&dl=0): predicted answers from 8 QA models
 - [qa_postprocess](https://www.dropbox.com/scl/fi/d8b8gljd8gvicm35lyzhr/qa_postprocess.zip?rlkey=w3nikidu7qg7zasdnobqshcli&st=3qrtm9b2&dl=0): postprocess on the predicted answers
-- [mistral-v0.3](https://www.dropbox.com/scl/fi/h64voic4glt7bfrwrq60l/mistral-v0.3.zip?rlkey=xjfl8nkhzu8gab0qmwyz389t8&st=sl8kfiay&dl=0): judged by mistral-v0.3
-- [llama-3.3-70b](https://www.dropbox.com/scl/fi/7extdn545nlu68trkb1fg/llama-3.3-70b.zip?rlkey=j2nlt74h0c54vrrmndkhoxl2g&st=ufghdtea&dl=0): judged by llama-3.3-70b
-- [qwen-2.5-72b](https://www.dropbox.com/scl/fi/73pamu4rnc1846jue3z97/qwen-2.5-72b.zip?rlkey=uk04lcoyj96oq5sdara7zx94i&st=zx0relcz&dl=0): judged by qwen-2.5-72b
-
+- [judge/mistral-v0.3](https://www.dropbox.com/scl/fi/5i88veb30xqbbj7bn0w71/judge.zip?rlkey=xobwku0k5m70vw74y3ssmztih&st=8n8eyb9b&dl=0): judged by mistral-v0.3
+- [judge/llama-3.3-70b](https://www.dropbox.com/scl/fi/5i88veb30xqbbj7bn0w71/judge.zip?rlkey=xobwku0k5m70vw74y3ssmztih&st=8n8eyb9b&dl=0): judged by llama-3.3-70b
+- [judge/qwen-2.5-72b](https://www.dropbox.com/scl/fi/5i88veb30xqbbj7bn0w71/judge.zip?rlkey=xobwku0k5m70vw74y3ssmztih&st=8n8eyb9b&dl=0): judged by qwen-2.5-72b
 
 
 
--- get_correlation_score.py
+++ get_correlation_score.py
@@ -22,8 +22,8 @@ model_mapping = {
 def get_ids_use(data_human):
     return [item["_id"] for item in data_human]
 
-human_file = "data/human_result.json"
 
+human_file = "data/human_result.json"
 with open(human_file, "r", encoding="utf-8") as f:
     data_human = json.load(f)
 
@@ -77,6 +77,51 @@ def cal_correlation_score(qa_model, judge_model):
     return score 
 
 
+def cal_correlation_score_em_f1(qa_model, judge_model):
+    """
+    qa_model = llama-3.1-8b (must be one in the list of 8 QA models)
+    judge_model = any model, just for getting em and f1 scores
+    """
+    qa_model_id = model_mapping[qa_model]
+
+    input_subdir = os.path.join(INPUT_DIR, judge_model)
+
+    all_pred_data = []
+    for dataset_name in DATASET_NAMES:
+        input_filename = f"{qa_model}_{dataset_name}.json"
+        input_path = os.path.join(input_subdir, input_filename)
+        # print(input_path)
+        if not os.path.exists(input_path):
+            print(f"[!] File not found: {input_path}")
+            continue
+
+        with open(input_path, "r", encoding="utf-8") as f:
+            all_pred_data += json.load(f)
+
+    # 
+    id_to_item = {item["_id"]: item for item in all_pred_data}
+    pred_use = [id_to_item[i] for i in ids_use_human]
+    #
+    assert len(pred_use) == len(data_human), "Lists are not the same length."
+    human_label = []
+    em_label = []
+    f1_label = []
+    for idx, (item1, item2) in enumerate(zip(pred_use, data_human)):
+        id1 = item1.get("_id")
+        id2 = item2.get("_id")
+        assert id1 == id2, f"Mismatch at index {idx}: {id1} != {id2}"
+        
+        human_label.append(int(item2[f"human_label_{qa_model_id}"]))
+        #
+        em_label.append(1 if item1["EM"] == True else 0)
+        # print(em_label)
+        f1_label.append(1 if item1["f1"] >=0.5 else 0)
+
+    score_em = round(pearson_correlation(human_label, em_label), 3)
+    score_f1 = round(pearson_correlation(human_label, f1_label), 3)
+    return score_em, score_f1 
+
+
 # QA models
 MODEL_NAMES = [
     "Mistral-7B",
@@ -89,8 +134,30 @@ MODEL_NAMES = [
     "Llama-3.1-70B"
 ]
 
-judge_model = "llama-3.3-70b" # mistral-v0.3 or llama-3.3-70b or qwen-2.5-72b 
+if __name__ == "__main__":
+    # ====== correlation scores for em and f1
+    print("=======This is correlation scores for EM and F1 ==============")
+    judge_model = "qwen-2.5-72b" # mistral-v0.3 or llama-3.3-70b or qwen-2.5-72b 
+    average_em, average_f1 = 0, 0
+    for qa_model in MODEL_NAMES:
+        print(f"QA model: {qa_model}")
+        em, f1 = cal_correlation_score_em_f1(qa_model.lower(), judge_model)
+        average_em += em
+        average_f1 += f1
+        print(f"EM: {em} ==== F1: {f1}")
+    print(f"Average EM: {round(average_em/len(MODEL_NAMES), 3)}")
+    print(f"Average F1: {round(average_f1/len(MODEL_NAMES), 3)}")
 
-for qa_model in MODEL_NAMES:
-    print(qa_model)
-    print(cal_correlation_score(qa_model.lower(), judge_model))
+    # 
+    # ====== correlation scores for llm-as-a-judge
+    JUDGE_NAMES = ["mistral-v0.3", "llama-3.3-70b", "qwen-2.5-72b"]
+
+    # judge_model = "llama-3.3-70b" # mistral-v0.3 or llama-3.3-70b or qwen-2.5-72b 
+    for judge_model in JUDGE_NAMES: 
+        print(f"=======This is correlation scores for Judge model: {judge_model}==============")
+        average = 0
+        for qa_model in MODEL_NAMES:
+            print(f"QA model: {qa_model}")
+            average += cal_correlation_score(qa_model.lower(), judge_model)
+            print(cal_correlation_score(qa_model.lower(), judge_model))
+        print(f"Average: {round(average/len(MODEL_NAMES), 3)}")
